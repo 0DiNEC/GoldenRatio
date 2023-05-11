146c196c9cd6bc7c0e13692cb541c3271a62c7b5
0DiNEC <as.odinson@gmail.com>
2023-05-11 12:52:13 +0300

    refactor: add comments

--- MinOneVariableGoldenRatio/Program.cs
+++ MinOneVariableGoldenRatio/Program.cs
@@ -5,17 +5,26 @@ namespace goldenRatio
     public class GoldenSectionSearch
     {
         private const double Phi = 1.618033988749895; // Золотое сечение
+
+        // Метод для поиска минимума функции одной переменной методом золотого сечения
+        /// <param name="a">Начальное значение интервала.</param>
+        /// <param name="b">Конечное значение интервала.</param>
+        /// <param name="epsilon">Точность.</param>
+        /// <returns>Значение минимума функции.</returns>
         public static double FindMinimum(Func<double, double> function, double a, double b, double epsilon)
         {
+            // Инициализация переменных для значения x1, x2, f1, f2
             double x1 = b - (b - a) / Phi;
             double x2 = a + (b - a) / Phi;
             double f1 = function(x1);
             double f2 = function(x2);
 
+            // Цикл выполняется до достижения необходимой точности
             while (Math.Abs(b - a) > epsilon)
             {
                 if (f1 < f2)
                 {
+                    // Обновление значений интервала, x2, f2, x1, f1 в случае, когда f1 < f2
                     b = x2;
                     x2 = x1;
                     f2 = f1;
@@ -24,6 +33,7 @@ namespace goldenRatio
                 }
                 else
                 {
+                    // Обновление значений интервала, x1, f1, x2, f2 в случае, когда f1 >= f2
                     a = x1;
                     x1 = x2;
                     f1 = f2;
@@ -31,12 +41,13 @@ namespace goldenRatio
                     f2 = function(x2);
                 }
             }
-
+            // Возврат найденного минимума как среднего значения интервала
             return (a + b) / 2;
         }
     }
     internal class Program
     {
+        // Пример функции, для которой ищем минимум (квадрат функции)
         public static double PowFunction(double x)
         {
             return x * x;
@@ -57,12 +68,16 @@ namespace goldenRatio
             if (!Double.TryParse(Console.ReadLine(), out b))
                 b = 1;
 
-            Console.Write("Введите необходимую точность по умолчанию (0.0001)  : ");
+            Console.Write("Введите необходимую точность по умолчанию (0.001)  : ");
             if (!Double.TryParse(Console.ReadLine(), out epsilon))
-                epsilon = 0.0001;
+                epsilon = 0.001;
+
+            if (epsilon <= 0)
+                epsilon = 0.001;
 
             try
             {
+                // Вызов метода поиска минимума функции и вывод результата
                 double minimumF = GoldenSectionSearch.FindMinimum(PowFunction, a, b, epsilon);
                 Console.WriteLine("Минимум функции: " + minimumF);
             }
@@ -71,7 +86,7 @@ namespace goldenRatio
                 System.Console.WriteLine("Произошла ошибка: " + ex.Message);
             }
 
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
